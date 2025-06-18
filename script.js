425b3dc14558163d97b69826b13dc614388a944e
Lauretee01 <laura.silva05052008@gmail.com>
2025-06-18 08:50:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function enviar () {
+    alert("Cadastro feito com sucesso!")
+}
+//VARIÁVEL PARA PEGAR O NOME DO VISITANTE
+var usuario = prompt ("Digite o seu nome")
+
+//PEGAR O VALOR DA VARIÁVEL E IMPRIMIR NA TELA
+document.getElementById('visitante').innerHTML = usuario
